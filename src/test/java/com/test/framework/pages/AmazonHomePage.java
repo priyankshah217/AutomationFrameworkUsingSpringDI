bcbe61056c0b146698db1231a1d30ce5ccc82a64
Priyank Shah <priyank.shah@coupa.com>
2020-06-19 18:03:45 +0530

    Removed unnecessary code

--- src/test/java/com/test/framework/pages/AmazonHomePage.java
+++ src/test/java/com/test/framework/pages/AmazonHomePage.java
@@ -4,20 +4,14 @@ import com.test.framework.annotations.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 @PageObject
 public class AmazonHomePage extends BasePage {
-    @FindBy(id = "twotabsearchtextbox")
-    private WebElement amazonSearchTextbox;
 
     public void enterAmazonSearch() {
         WebElement searchTextBox = driverFactory.getDriver().findElement(By.id("twotabsearchtextbox"));
         searchTextBox.sendKeys("Computer");
         searchTextBox.sendKeys(Keys.RETURN);
-
-//        amazonSearchTextbox.sendKeys("Computer");
-//        amazonSearchTextbox.sendKeys(Keys.RETURN);
     }
 
 
--- src/test/java/com/test/framework/pages/GoogleHomePage.java
+++ src/test/java/com/test/framework/pages/GoogleHomePage.java
@@ -4,20 +4,13 @@ import com.test.framework.annotations.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 @PageObject
 public class GoogleHomePage extends BasePage {
-    @FindBy(name = "q")
-    private WebElement searchTextbox;
 
     public void enterGoogleSearch() {
         WebElement searchTextBox = driverFactory.getDriver().findElement(By.name("q"));
         searchTextBox.sendKeys("Selenium");
         searchTextBox.sendKeys(Keys.RETURN);
-
-//        searchTextbox.sendKeys("Selenium");
-//        searchTextbox.sendKeys(Keys.RETURN);
     }
-
 }
